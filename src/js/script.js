6a4db489da568404cd9b9cd56ebe2d9211fd803f
Phil <kshaadoo1991@gmail.com>
2023-05-31 11:01:02 +0300

    adapted website for mobile >=767px

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger'),
-    sub = document.querySelector('.subheader'),
-    promo = document.querySelector('.promo'),
-    promoSecond = document.querySelector('.promo_second');
+    hamburger = document.querySelector('.hamburger');
+
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
     });
 
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            });
         });
-    });
 });
